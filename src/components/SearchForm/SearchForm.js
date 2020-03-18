9fcd5a523bdc4c8fb5e5e8c5dd57b4009cb802b9
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-03-18 01:54:33 -0600

    Add class and functionality for SearchForm

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,6 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './SearchForm.css';
 
 // SEARCHFORM COMPONENT CODE GOES HERE
+
+class SearchForm extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      searchWord: ""
+    }
+  }
+
+  filteredTopic = () => {
+
+  }
+
+  render() {
+    return(
+      <form>
+        <input
+        name="searchWord"
+        placeholder="Search Article"
+        onChange={this.updateState}
+        value={this.state.searchWord}
+        />
+        <button>Search</button>
+      </form>
+    )
+  }
+
+
+}
 
 export default SearchForm;
