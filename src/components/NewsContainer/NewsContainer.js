4756fabcf91f00e6570ea42515dcb58a9e49c3e0
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-03-19 16:46:27 -0600

    Refactor code into helper functions

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -3,21 +3,22 @@ import NewsArticle from "../NewsArticle/NewsArticle"
 import './NewsContainer.css'
 
 const NewsContainer = (props) => {
+  const articleDisplay = props.newsData.map(article =>
+      (
+        <NewsArticle
+        id={article.id}
+        key={article.id}
+        img={article.img}
+        headline={article.headline}
+        description={article.description}
+        url={article.url}
+        />
+      )
+    )
+
   return(
     <section className="news-container">
-    {props.newsData.map(article =>
-        (
-          <NewsArticle
-          id={article.id}
-          key={article.id}
-          img={article.img}
-          headline={article.headline}
-          description={article.description}
-          url={article.url}
-          />
-        )
-      )
-    }
+      {articleDisplay}
     </section>
   )
 }
